6fca763f810243297abca740f1c53e8e23634236
Kevin Clement <kevin.clement@gmail.com>
2022-10-11 19:23:05 -0700

    add LED Control

---
+++ modes/switch_light_control/code/switch_light_control.py
@@ -0,0 +1,26 @@
+from mpf.core.mode import Mode
+from os.path import exists
+import time
+
+class Trigger_Pi(Mode):
+
+    def write_file(fname, txt):
+        f = open(fname, "w")
+        f.write(txt)
+        f.close()
+
+    def mode_start(self, **kwargs):
+        self.log.debug("Trigger_Pi mode start")
+
+        if exists("/sys/class/gpio/export"): 
+            self.log.debug("Found gpio export, turning off LED Control")
+            
+            write_file("/sys/class/gpio/export", "17")
+            write_file("/sys/class/gpio/gpio17/direction", "out")
+            write_file("/sys/class/gpio/gpio17/value", "1")
+            time.sleep(.4)
+            write_file("/sys/class/gpio/gpio17/value", "0")
+            write_file("/sys/class/gpio/unexport", "17")
+        else: 
+            self.log.debug("NO gpio found, ignorning LED Control")
+    
