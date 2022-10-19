62acb407c8899dc53a3fd4f8a21bbab72509ffac
Kevin Clement <kevin.clement@gmail.com>
2022-10-19 03:02:29 -0700

    more natural end of ball bonus calculator

--- modes/bonus/code/bonus.py
+++ modes/bonus/code/bonus.py
@@ -1,66 +1,50 @@
 from mpf.core.mode import Mode
-from mpf.core.delays import DelayManager
-from mpf.core.utility_functions import Util
+import asyncio
 
+# Bonus mode trying to emulate what happens in the real EM machine
+#   See Vid Camps Sure Shot clipped: https://youtube.com/clip/UgkxlNKh9j2blmnCA238TiZ1xtqN9AcK2sho
 class Count(Mode):
     def mode_start(self, **kwargs):
         self.machine.events.post("bonus_mode_start")
-        # if self.player['ball'] == 3:
-        #    self.bonus_value = 2000
-        # else:
-        #    self.bonus_value = 1000
+        
+        self.bonus_value = 1000
+        self.count_down = self.player["total_balls_collected"]
+        self.total_run = 0
 
-        # self.count_down = self.machine.counters.lb_bonus.value
-        # self.machine.events.post("bonus_start" + str(self.count_down))
-        self.prepare_bonus()
-
-    def prepare_bonus(self):
-        self.machine.events.post("prepare_bonus")
-        # self.machine.events.post('stop_bonus_shows')
-
-        # for loop in range(1000, self.count_down + 1000, 1000):
-        #     self.machine.lights['pfl_bonus_' + str(loop)].on()
-
-        self.check_score_reels()
+        self.machine.events.post("bonus_start" + str(self.count_down))
+        
+        # Delay the calculation by 1s to simulate EM natural delay
+        self.delay.add(1000, self.check_score_reels)
 
     def check_score_reels(self, **kwargs):
         self.machine.events.post("bonus_code_check_score_reels_valid")
 
-        # TMP: TODO: REMOVE
-        self.bonus_step()
-
-        # if self.player['player_number'] == 1:
-        #     ready_future = Util.ensure_future(self.machine.score_reel_groups.player1.wait_for_ready(), loop=self.machine.clock.loop)
-        # else:
-        #     ready_future = Util.ensure_future(self.machine.score_reel_groups.player2.wait_for_ready(), loop=self.machine.clock.loop)
-
-        # ready_future.add_done_callback(self.bonus_step)
+        ready_future = asyncio.ensure_future(self.machine.score_reel_groups.srg_score.wait_for_ready(), loop=self.machine.clock.loop)
+        ready_future.add_done_callback(self.bonus_step)
 
     def bonus_step(self, future=None, **kwargs):
         self.machine.events.post("bonus_code_step")
 
-        # TMP: TODO: REMOVE
-        self.bonus_pause()
+        # add a slight delay every 5k, to simulate what EM machine did
+        delay = 0
+        if (self.total_run > 0 and self.total_run % 5 == 0):
+            delay = 150
 
-        # if self.count_down > 0:
-        #     self.delay.add(ms=500, callback=self.do_bonus_step)
-        # else:
-        #     self.bonus_pause()
+        if self.count_down > 0:
+            self.delay.add(ms=delay, callback=self.do_bonus_step)
+        else:
+            self.bonus_pause()
             
     def do_bonus_step(self):
-        self.machine.events.post("do_bonus_step")
-        # self.machine.events.post("bonus_code_countdown_" + str(self.count_down))
-        # self.player.score += self.bonus_value
+        self.machine.events.post("bonus_code_countdown_" + str(self.count_down))
+        self.player.score += self.bonus_value
 
-        # self.machine.shows['flash'].play(show_tokens=dict(light='pfl_bonus_' + str(self.count_down)), speed=10.0, loops=3)
-        # self.machine.lights['pfl_bonus_' + str(self.count_down)].off()
-
-        # self.count_down -= 1000
-
-        # self.check_score_reels()
+        self.count_down -= 1
+        self.total_run += 1
+        self.check_score_reels()
 
     def bonus_pause(self):
-        self.machine.events.post("bonus_pause")
+        # pause for 1s to have a natural end of bonus
         self.delay.add(ms=1000, callback=self.bonus_done)
 
     def bonus_done(self):
